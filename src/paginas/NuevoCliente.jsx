373db710564f1ea301aead491c6f569b6a6eb792
Arlez <rainmake123@gmail.com>
2022-07-19 20:38:18 -0400

    avance formulario

--- src/paginas/NuevoCliente.jsx
+++ src/paginas/NuevoCliente.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import Formulario from "../components/Formulario"
 
 const NuevoCliente = () => {
   return (
-    <div>
-        <h1>nuevocliente.jsx</h1>
-    </div>
+    <>
+        <h1 className='font-black text-4xl text-blue-900'>Nuevo Cliente</h1>
+        <p>Llena los siguientes campos para registrar un cliente</p>
+        <Formulario/>
+    </>
   )
 }
 
