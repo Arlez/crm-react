30f23e0aeb23ed905b66072c6a98427b5bd99326
Daniel Barrera <dbarrera.ap@gmail.com>
2022-07-28 17:43:01 -0400

    termino - mostrar cliente

--- src/components/Cliente.jsx
+++ src/components/Cliente.jsx
@@ -15,7 +15,7 @@ const Cliente = ({cliente}) => {
             </td>
             <td className='p-3'>{empresa}</td>
             <td className='p-3'>
-                <button type='button' className='bg-yellow-500 hover:bg-yellow-600 block w-full text-white p-2 uppercase font-bold text-xs' onClick={()=>navigate('/clientes/'+id)}>Ver</button>
+                <button type='button' className='bg-yellow-500 hover:bg-yellow-600 block w-full text-white p-2 uppercase font-bold text-xs' onClick={()=>navigate(`/clientes/${id}`)}>Ver</button>
                 <button type='button' className='bg-blue-600 hover:bg-blue-700 block w-full text-white p-2 uppercase font-bold text-xs mt-3'>Editar</button>
                 <button type='button' className='bg-red-600 hover:bg-red-700 block w-full text-white p-2 uppercase font-bold text-xs mt-3'>Eliminar</button>
             </td>
